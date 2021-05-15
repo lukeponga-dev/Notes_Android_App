84014c3bef73ccc963cea222e407a205c3fa737e
luke gilchrist <lukegilchrist69@hotmail.com>
2021-05-15 22:32:44 +1200

    Add project files.

---
+++ Notes_Android_App/Notes_Android_App/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Notes_Android_App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Notes_Android_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(NoteEntryPage), typeof(NoteEntryPage));
+        }
+    }
+
+}
+
---
+++ Notes_Android_App/Notes_Android_App/Views/NotesPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Notes_Android_App.Models;
+using Xamarin.Forms;
+
+namespace Notes_Android_App.Views
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Retrieve all the notes from the database, and set them as the
+            // data source for the CollectionView.
+            collectionView.ItemsSource = await App.Database.GetNotesAsync();
+
+/*                        var notes = new List<Note>();
+*/
+            // Create a Note object from each file.
+
+
+
+            // Set the data source for the CollectionView to a
+            // sorted collection of notes.
+/*            collectionView.ItemsSource = await App.Database.GetNotesAsync();
+*/         
+        }
+
+        private async void OnAddClicked(object sender, EventArgs e)
+        {
+            // Navigate to the NoteEntryPage, without passing any data.
+            await Shell.Current.GoToAsync(nameof(NoteEntryPage));
+        }
+
+        async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
+            {
+                // Navigate to the NoteEntryPage, passing the ID as a query parameter.
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.ID.ToString()}");
+            }
+        }
+    }
+}
